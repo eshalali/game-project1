d32b499db881e135b85a822d8f6f1aa04a3244cc
eshalali <eshal.a.ali@gmail.com>
2022-06-23 15:54:37 -0500

    Create title and image banks and create pop up question div

--- js/app.js
+++ js/app.js
@@ -1,41 +1,105 @@
 const button = document.querySelector('button')
 const questionCategories = ['Renaissance', 'Classicism', 'Impressionism', 'Modernism', 'Realism']
+const modal = document.querySelector(".modal")
+const span = document.getElementsByClassName("close")[0]
 
 const onGetSearchFailure = () => {
     console.log('failed')
 }
 
-const addToRenAnswers = (objectinfo) => {
-    const renAnswers = []
-    renAnswers.push(objectinfo.title)
-    console.log(renAnswers)
-}
-
+// creating renaissance title and image banks
 const onGetRenSuccess = (searchResult) => {
     console.log(searchResult)
-    resultIds = []
-    searchResult.objectIDs.forEach((result) => {
-        resultIds.push(result)
+    const renTitles = []
+    const renPhotos = []
+    searchResult.data.forEach((result) => {
+        renTitles.push(result.title)
+        renPhotos.push(`https://www.artic.edu/iiif/2/${result.id}/full/200,/0/default.jpg`)
     })
-    console.log(resultIds)
-    for (let i = 0; i < 10; i++) {
-        fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/[${resultIds[i]}]`)
-            .then(res => res.json())
-            .then(addToRenAnswers)
-            .catch(onGetSearchFailure)
-    }
+    console.log(renPhotos[0])
 }
 
-fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?q=renaissance")
+fetch("https://api.artic.edu/api/v1/artworks/search?q=renaissance&limit=100")
     .then(res => res.json())
     .then(onGetRenSuccess)
     .catch(onGetSearchFailure)
-// const classicAnswers = []
-// const impressionAnswers = []
-// const modernAnswers = []
-// const realismAnswers = []
+
+// creating classicism bank
+const onGetClassicSuccess = (searchResult) => {
+    console.log(searchResult)
+    const classicTitles = []
+    const classicPhotos = []
+    searchResult.data.forEach((result) => {
+        classicTitles.push(result.title)
+        classicPhotos.push(`https://www.artic.edu/iiif/2/${result.id}/full/200,/0/default.jpg`)
+    })
+    console.log(classicTitles)
+}
+
+fetch("https://api.artic.edu/api/v1/artworks/search?q=classicism&limit=100")
+    .then(res => res.json())
+    .then(onGetClassicSuccess)
+    .catch(onGetSearchFailure)
+
+// creating impressionism bank
+const onGetImpressionSuccess = (searchResult) => {
+    console.log(searchResult)
+    const impressionTitles = []
+    const impressionPhotos = []
+    searchResult.data.forEach((result) => {
+        impressionTitles.push(result.title)
+        impressionPhotos.push(`https://www.artic.edu/iiif/2/${result.id}/full/200,/0/default.jpg`)
+    })
+    console.log(impressionTitles)
+}
+
+fetch("https://api.artic.edu/api/v1/artworks/search?q=impressionism&limit=100")
+    .then(res => res.json())
+    .then(onGetImpressionSuccess)
+    .catch(onGetSearchFailure)
+
+// creating modernism bank
+const onGetModernSuccess = (searchResult) => {
+    console.log(searchResult)
+    const modernTitles = []
+    const modernPhotos = []
+    searchResult.data.forEach((result) => {
+        modernTitles.push(result.title)
+        modernPhotos.push(`https://www.artic.edu/iiif/2/${result.id}/full/200,/0/default.jpg`)
+    })
+    console.log(modernTitles)
+}
+
+fetch("https://api.artic.edu/api/v1/artworks/search?q=modernism&limit=100")
+    .then(res => res.json())
+    .then(onGetModernSuccess)
+    .catch(onGetSearchFailure)
+
+// creating realism bank
+const onGetRealismSuccess = (searchResult) => {
+    console.log(searchResult)
+    const realTitles = []
+    const realPhotos = []
+    searchResult.data.forEach((result) => {
+        realTitles.push(result.title)
+        realPhotos.push(`https://www.artic.edu/iiif/2/${result.id}/full/200,/0/default.jpg`)
+    })
+    console.log(realTitles)
+}
+
+fetch("https://api.artic.edu/api/v1/artworks/search?q=realism&limit=100")
+    .then(res => res.json())
+    .then(onGetRealismSuccess)
+    .catch(onGetSearchFailure)
+
 
 button.addEventListener('click', function() {
     chosenCategory = questionCategories[Math.floor(Math.random() * 4)]
     console.log(chosenCategory)
+    modal.style.display = "block"
 })
+
+span.onclick = function() {
+    modal.style.display = "none"
+  }
+  
