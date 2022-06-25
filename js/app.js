de80eab3b94659449e01a4de5dccfa3eedfe9dcb
eshalali <eshal.a.ali@gmail.com>
2022-06-25 17:00:16 -0500

    Remove event listeners from answers choices after each question

--- js/app.js
+++ js/app.js
@@ -8,32 +8,89 @@ const answer2 = document.getElementById('answer2')
 const answer3 = document.getElementById('answer3')
 const answer4 = document.getElementById('answer4')
 const answers = [answer1, answer2, answer3, answer4]
+const timer = document.getElementById('timer')
+const question = document.getElementById('question')
+const player1 = document.getElementById('player1')
+const player2 = document.getElementById('player2')
+
+let player1Score = 0
+let player2Score = 0
+let turn = 1
+
+let cateTitles = []
+let catePhotos = []
+
+let rightAnswer = NaN
 
 const onGetSearchFailure = () => {
     console.log('failed')
 }
+
+const rightChoice = () => {
+    console.log('correct')
+    answers[rightAnswer].style.backgroundColor = 'lightgreen'
+    question.innerText = 'Correct!'
+
+    if (turn % 2 === 1) {
+        player1Score += 20
+        turn++
+        if (player1Score === 100) {
+            console.log('Player 1 wins!')
+        }
+    } else {
+        player2Score += 20
+        turn++
+        if (player2Score === 100) {
+            console.log(['Player 2 wins!'])
+        }
+    }
+}
+const wrongChoice = (event) => {
+    console.log('wrong')
+    event.target.style.backgroundColor = 'red'
+    question.innerText = 'Incorrect!'
+    turn++
+    }
+    
+
+
+const gamefunction = () => {
+    let questionIndex = Math.floor(Math.random() * (catePhotos.length-1))
+    questionPic.src = catePhotos[questionIndex]
+    catePhotos.splice(questionIndex, 1)
+    rightAnswer = Math.floor(Math.random() * 3)
+    const answers = [answer1, answer2, answer3, answer4]
+    console.log(rightAnswer)
+    answers[rightAnswer].innerText = cateTitles[questionIndex]
+    cateTitles.splice(questionIndex, 1)
+    console.log(turn)
+    answers[rightAnswer].addEventListener('click', rightChoice)
+    for (let i = 0; i < answers.length; i++) {
+        if (i !== rightAnswer) {
+            answers[i].innerText = cateTitles[Math.floor(Math.random() * (cateTitles.length-1))]
+            answers[i].addEventListener('click', wrongChoice)
+        }
+    }
+}
+const reset = () => {
+    answers[rightAnswer].removeEventListener('click', rightChoice)
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].removeEventListener('click', wrongChoice)
+    rightAnswer = NaN
+   
+}
+}
+
 // creating title and picture bank
 const onGetSearchSuccess = (searchResult) => {
     console.log(searchResult)
-    let cateTitles = []
-    let catePhotos = []
     searchResult.data.forEach((result) => {
         cateTitles.push(result.title)
         catePhotos.push(`https://www.artic.edu/iiif/2/${result.image_id}/full/843,/0/default.jpg`)
     })
-    let questionIndex = Math.floor(Math.random() * (catePhotos.length-1))
-    questionPic.src = catePhotos[questionIndex]
-    catePhotos.splice(questionIndex)
-    let rightAnswer = Math.floor(Math.random() * 3)
-    console.log(rightAnswer)
-    answers[rightAnswer].innerText = cateTitles[questionIndex]
-    cateTitles.splice(questionIndex)
-    answers.splice(rightAnswer)
-    for (let i = 0; i < 3; i++) {
-        answers[i].innerText = cateTitles[Math.floor(Math.random() * (cateTitles.length-1))]
-    }
+    // answers.splice(rightAnswer, 1)
+    gamefunction()
 }
-
 
 button.addEventListener('click', function() {
     chosenCategory = questionCategories[Math.floor(Math.random() * 4)]
@@ -67,6 +124,12 @@ button.addEventListener('click', function() {
 })
 
 span.onclick = function() {
+    reset()
     card.style.display = "none"
+    for (i = 0; i < answers.length; i++) {
+        answers[i].style.backgroundColor = 'bisque'
+    }
+    player1.innerText = `Player 1 Score: ${player1Score}`
+    player2.innerText = `Player 2 Score: ${player2Score}`
   }
-  
+
